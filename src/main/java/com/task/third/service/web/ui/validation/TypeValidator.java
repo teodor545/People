8bde0736b64e0137dc8685f96447fd0da441ac27
teodor545 <teodorvlv@gmail.com>
2024-06-13 20:48:38 +0300

    Switch to pattern

--- src/main/java/com/task/third/service/web/ui/validation/TypeValidator.java
+++ src/main/java/com/task/third/service/web/ui/validation/TypeValidator.java
@@ -4,11 +4,15 @@ import com.vaadin.flow.data.binder.ValidationResult;
 import com.vaadin.flow.data.binder.Validator;
 import com.vaadin.flow.data.binder.ValueContext;
 
+import java.util.regex.Pattern;
+
 public class TypeValidator implements Validator<String> {
+
+    private final Pattern typePattern = Pattern.compile("^[a-zA-Z]{1,5}$");
 
     @Override
     public ValidationResult apply(String value, ValueContext context) {
-        if (value!= null && value.length() > 0 && value.length() <= 5) {
+        if (typePattern.matcher(value).find()) {
             return ValidationResult.ok();
         } else {
             return ValidationResult.error("Type must be between 1 and 5 characters");
